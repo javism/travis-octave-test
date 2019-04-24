1d94bc6ac595a2a34d54ddacef90d71ed63bb14e
Javier Sanchez <javism@posteo.net>
2019-04-25 00:30:14 +0200

    re-fixed

--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -1,12 +1,5 @@
 import subprocess
 
-subprocess.call(["ls", "-l"])
-
-## command to run - tcp only ##
-#cmd = "jupyter nbconvert --to script example-notebook.ipynb" ## run it ##
-#result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-#text = result.communicate()[0]
-#returncode = result.returncode
 cmd = ["jupyter","nbconvert", "--to", "script", "example-notebook.ipynb"]
 returncode = subprocess.call(cmd)
 
@@ -15,12 +8,7 @@ print("returncode:" + str(returncode))
 if returncode != 0:
 	exit(returncode)
 
-#cmd = "octave-cli example-notebook.m" ## run it ##
-#result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-#text = result.communicate()[0]
-#returncode = result.returncode
-
-cmd = ["octave-cli", "example-notebook.m"]
+cmd = ["octave-cli", "example_notebook.m"]
 returncode = subprocess.call(cmd)
 print("returncode:" + str(returncode))
 
--- notebooks/test_notebooks.sh
+++ notebooks/test_notebooks.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
-#jupyter nbconvert --to script example-notebook.ipynb
-#octave-cli example-notebook.m
-octave-cli example_notebook.m
+jupyter nbconvert --to script example-notebook.ipynb
+octave-cli example-notebook.m
+#octave-cli example_notebook.m
 status=$?
 echo 'status'
 echo $status
-
 if [ $status -eq 0 ]
 then
-  echo "ok!"
+  echo "ok!!"
   exit $status
 else
-  echo "error" 
+  echo "error!!" 
   exit $status
 fi
