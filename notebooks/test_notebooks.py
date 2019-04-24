e08b5515dc8088db825d80dcb83cf1676eee6415
Javier Sanchez <javism@posteo.net>
2019-04-24 17:23:52 +0200

    Compatibility with python 2.7

--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -1,19 +1,27 @@
-import subprocess, sys
+import subprocess
+
+subprocess.call(["ls", "-l"])
+
 ## command to run - tcp only ##
-cmd = "jupyter nbconvert --to script example-notebook.ipynb" ## run it ##
-result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-text = result.communicate()[0]
-returncode = result.returncode
+#cmd = "jupyter nbconvert --to script example-notebook.ipynb" ## run it ##
+#result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+#text = result.communicate()[0]
+#returncode = result.returncode
+cmd = ["jupyter","nbconvert", "--to", "script", "example-notebook.ipynb"]
+returncode = subprocess.call(cmd)
+
 print("returncode:" + str(returncode))
 
 if returncode != 0:
 	exit(returncode)
 
+#cmd = "octave-cli example-notebook.m" ## run it ##
+#result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+#text = result.communicate()[0]
+#returncode = result.returncode
 
-cmd = "octave-cli example-notebook.m" ## run it ##
-result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-text = result.communicate()[0]
-returncode = result.returncode
+cmd = ["octave-cli", "example-notebook.m"]
+returncode = subprocess.call(cmd)
 print("returncode:" + str(returncode))
 
 exit(returncode)
